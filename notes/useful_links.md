87c5fac56cf191b8408806af9d00473ab23ab986
Anders Black Larsen <ablack@pop-os.localdomain>
2023-09-13 09:09:28 +0200

    fish_parenteses

--- notes/useful_links.md
+++ notes/useful_links.md
@@ -1,3 +1,3 @@
 ** http://users.cecs.anu.edu.au/~patrik/pddlman/writing.html
 ** https://adriann.github.io/rust_parser.html 
-** https://stackoverflow.com/questions/67722023/how-to-use-rust-nom-to-write-a-parser-for-this-kind-of-structure-text 
+** https://stackoverflow.com/questions/67722023/how-to-use-rust-nom-to-write-a-parser-for-this-kind-of-structure-text
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,18 +2,18 @@ use  crate::problem::Problem;
 use nom::IResult;
 use nom::branch::alt;
 use nom::bytes::complete::{tag, take_while, take_until};
-use nom::sequence::{terminated, delimited, pair};
-use nom::multi::{separated_list0, many1};
+use nom::sequence::{terminated, delimited, pair, preceded};
+use nom::multi::{separated_list0, many1, many0};
 use parse_hyperlinks::take_until_unbalanced;
 
 pub fn parse( input: &str ) -> IResult<&str, &str> {
 
   let (mut input, mut res) = delimited(tag("("), take_until_unbalanced('(', ')'), tag(")"))(input)?;
 
-  while res.len() != 0 {
-    (input, res) = fish_parenteses(res)?;
-    println!("{}", res);
-  }
+  let (mut input, vec_res) = fish_parenteses(res)?;
+
+  println!("Result: {:?}", vec_res);
+
 
   // let prob = Problem { 
 
@@ -23,12 +23,11 @@ pub fn parse( input: &str ) -> IResult<&str, &str> {
 
 }
 
-pub fn fish_parenteses( input: &str ) -> IResult<&str, &str> {
-  println!("Input: {}", input);
+pub fn fish_parenteses( input: &str ) -> IResult<&str, Vec<&str>> {
+  println!("to fp: {}", input);
 
-  let (input, res) = delimited(tag("("), take_until_unbalanced('(', ')'), tag(")"))(input)?;
+  let (remain, res) = many0( delimited(tag("("), take_until(")"), tag(")")) )("( tester )( tester1 )")?;
 
-  println!("res: {}", res);
 
-  IResult::Ok((input, res))
+  IResult::Ok((remain, res))
 }
